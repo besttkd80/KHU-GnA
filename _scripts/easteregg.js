9aa99ff57db73746374620a72f6aa11f88a0a4c4
jhw980212 <jhw980212@gmail.com>
2025-12-11 08:28:36 +0900

    Create easteregg.js

---
+++ _scripts/easteregg.js
@@ -0,0 +1,16 @@
+(function () {
+  let buffer = "";
+  const secret = "gna";
+
+  document.addEventListener("keydown", (e) => {
+    buffer += e.key.toLowerCase();
+    if (buffer.length > secret.length) {
+      buffer = buffer.slice(buffer.length - secret.length);
+    }
+
+    if (buffer === secret) {
+      alert("본 사이트 제작: 정형웅(메인) · 안예진(서브) · 송지영(서브)");
+      buffer = "";
+    }
+  });
+})();
